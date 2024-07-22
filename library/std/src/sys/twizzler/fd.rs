96cf961f30b249ec08007ea78cba4f01bef5802b
CPTforever <yester1324@gmail.com>
2024-07-22 17:14:33 +0000

    Added file descriptor code

---
+++ library/std/src/sys/twizzler/fd.rs
@@ -0,0 +1,84 @@
+use crate::io::{self, Read, SeekFrom};
+use crate::io::SeekFrom::{Start, Current, End};
+
+use crate::sys::unsupported;
+use twizzler_runtime_api::OwnedFd;
+use crate::sys_common::IntoInner;
+use crate::sys_common::FromInner;
+
+use twizzler_runtime_api::SeekFrom as InnerSeek;
+use twizzler_runtime_api::SeekFrom::{Start as InnerStart, Current as InnerCurrent, End as InnerEnd};
+
+// A abstraction that can do continious IO on a set of Twizzler objects
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
+pub struct FileDesc {
+    pub fd: OwnedFd
+}
+
+impl FileDesc {
+    pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
+        let runtime = twizzler_runtime_api::get_runtime();
+
+        let result = runtime.read(self.fd, buf.as_mut_ptr(), buf.len()).expect("Read");
+
+        Ok(result as usize)
+    }
+
+    pub fn read_to_end(&self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        let mut me = self;
+        (&mut me).read_to_end(buf)
+    }
+
+    pub fn write(&self, buf: &[u8]) -> io::Result<usize> {
+        let runtime = twizzler_runtime_api::get_runtime();
+
+        let result = runtime.write(self.fd, buf.as_ptr(), buf.len()).expect("Write");
+        
+        Ok(result as usize)
+    }
+
+    pub fn seek(&self, pos: SeekFrom) -> io::Result<u64> {
+        let runtime = twizzler_runtime_api::get_runtime();
+
+        let inner: InnerSeek = match pos {
+            Start(x) => InnerSeek::Start(x),
+            End(x) => InnerSeek::End(x),
+            Current(x) => InnerSeek::Current(x),
+        };
+
+        let result = runtime.seek(self.fd, inner).expect("Seek");
+
+        Ok(result as u64)
+    }
+
+    pub fn duplicate(&self) -> io::Result<FileDesc> {
+        self.duplicate_path(&[])
+    }
+    pub fn duplicate_path(&self, _path: &[u8]) -> io::Result<FileDesc> {
+        unsupported()
+    }
+
+    pub fn nonblocking(&self) -> io::Result<bool> {
+        Ok(false)
+    }
+
+    pub fn set_cloexec(&self) -> io::Result<()> {
+        unsupported()
+    }
+
+    pub fn set_nonblocking(&self, _nonblocking: bool) -> io::Result<()> {
+        unsupported()
+    }
+}
+
+impl IntoInner<OwnedFd> for FileDesc {
+    fn into_inner(self) -> OwnedFd {
+        self.fd
+    }
+}
+
+impl FromInner<OwnedFd> for FileDesc {
+    fn from_inner(owned_fd: OwnedFd) -> Self {
+        Self { fd: owned_fd }
+    }
+}
