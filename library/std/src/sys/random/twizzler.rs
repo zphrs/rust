89a9d2d9bd30cf90470fc7cadc162d2eb8d018b4
zphrs <z@zephiris.dev>
2024-12-29 15:07:11 -0800

    added support for random.

--- library/std/src/sys/random/twizzler.rs
+++ library/std/src/sys/random/twizzler.rs
@@ -1,6 +1,9 @@
 pub fn fill_bytes(mut bytes: &mut [u8]) {
     while !bytes.is_empty() {
-        let res = twizzler_rt_abi::random::twz_rt_get_random(bytes, twizzler_rt_abi::random::GetRandomFlags::empty());
+        let res = twizzler_rt_abi::random::twz_rt_get_random(
+            bytes,
+            twizzler_rt_abi::random::GetRandomFlags::empty(),
+        );
         if res == 0 {
             panic!("failed to fill entropy bytes");
         }
