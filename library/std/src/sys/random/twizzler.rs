1a9864f0d90b139300394bee765ec2ecb8be4683
zphrs <z@zephiris.dev>
2024-12-29 17:45:00 -0800

    undid unnecessary change.

--- library/std/src/sys/random/twizzler.rs
+++ library/std/src/sys/random/twizzler.rs
@@ -2,7 +2,7 @@ pub fn fill_bytes(mut bytes: &mut [u8]) {
     while !bytes.is_empty() {
         let res = twizzler_rt_abi::random::twz_rt_get_random(
             // SAFETY: `MaybeUninit<T>` is guaranteed to be layout-compatible with `T`.
-            &mut *(slice as *mut [u8] as *mut [MaybeUninit<u8>]),
+            bytes,
             twizzler_rt_abi::random::GetRandomFlags::empty(),
         );
         if res == 0 {
