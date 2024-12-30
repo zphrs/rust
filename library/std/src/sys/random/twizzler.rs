28d91531ded127fdb771ef6bc9356a329ffa9f82
zphrs <z@zephiris.dev>
2024-12-30 00:22:03 -0800

    randomness fix again

--- library/std/src/sys/random/twizzler.rs
+++ library/std/src/sys/random/twizzler.rs
@@ -1,8 +1,9 @@
+use core::mem::MaybeUninit;
 pub fn fill_bytes(mut bytes: &mut [u8]) {
     while !bytes.is_empty() {
         let res = twizzler_rt_abi::random::twz_rt_get_random(
             // SAFETY: `MaybeUninit<T>` is guaranteed to be layout-compatible with `T`.
-            bytes,
+            unsafe { &mut *(bytes as *mut [u8] as *mut [MaybeUninit<u8>]) },
             twizzler_rt_abi::random::GetRandomFlags::empty(),
         );
         if res == 0 {
